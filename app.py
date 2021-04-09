161305dbc57cb36e319a24edf1e908e04508bf0b
jbaird <jarredbaird@gmail.com>
2021-04-09 15:13:24 -0400

    added all templates needed. finished models. finish templates

--- app.py
+++ app.py
@@ -3,23 +3,65 @@ import os
 from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
+from flask import Flask
+from flask_mail import Mail, Message
+import imaplib
+import email
+from email.header import decode_header
+import webbrowser
 import pdb
 
 # from forms import UserAddForm, UserEditForm, LoginForm, MessageForm
 from models import db, connect_db
 
-CURR_USER_KEY = "curr_user"
-
 app = Flask(__name__)
+mail = Mail(app)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///gettingthingsdone'))
-
+    os.environ.get('DATABASE_URL', 'postgresql:///gtd'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "echnidna_eggs")
 toolbar = DebugToolbarExtension(app)
-
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = "jarredbaird@gmail.com"
+app.config['MAIL_PASSWORD'] = "cbqpzghnllzragpr"
+mail.init_app(app)
 connect_db(app)
+
+CURR_USER_KEY = "curr_user"
+MAPS_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places/23314.json?access_token=pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrN2Y1Nmp4YjB3aG4zZ253YnJoY21kbzkifQ.JM5ZeqwEEm-Tonrk5wOOMw&cachebuster=1616533965014&autocomplete=true"
+
+# account credentials
+username = "jarredbaird@gmail.com"
+password = "cbqpzghnllzragpr"
+
+@app.route('/')
+def home():
+    return render_template('focus.html')
+# def index():
+#     msg = Message("Hello",
+#                   sender=("jarjar", "jarredbaird@gmail.com"),
+#                   recipients=["jarredbaird@gmail.com"])
+#     assert msg.sender == "jarjar <jarredbaird@gmail.com>"
+#     msg.html = "<b>testing</b>"
+#     mail.send(msg)
+#     return "Sent!"
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+@app.route('/read-email')
+def read_email():
+    return 0
+
+def clean(text):
+    # create an IMAP4 class with SSL 
+    imap = imaplib.IMAP4_SSL("imap.gmail.com")
+    # authenticate
+    imap.login(username, password)
--- models.py
+++ models.py
@@ -13,3 +13,106 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+class User(db.Model):
+    """User Model"""
+
+    __tablename__ = "users"
+    u_id = db.Column(db.Integer, primary_key=True)
+    active = db.Column(db.Boolean, nullable=False)
+    email = db.Column(db.Text, nullable=False)
+    u_name = db.Column(db.Text, nullable=False)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Outcome(db.Model):
+    """The outcome of a specific item"""
+    __tablename__ = "outcomes"
+    o_id = db.Column(db.Integer, primary_key=True)
+    o_name = db.Column(db.Text, nullable=False)
+
+class NextStep(db.Model):
+    """The next step needed to get an item going"""
+    __tablename__ = "next_steps"
+    ns_id = db.Column(db.Integer, primary_key=True)
+    ns_name = db.Column(db.Text, nullable=False)
+
+class Item(db.Model):
+    """This item ends up in your inbox to be refined later"""
+    __tablename__ = "items"
+    i_id = db.Column(db.Integer, primary_key=True)
+    i_name = db.Column(db.Text, nullable=False)
+    u_id = db.Column(db.Integer, db.ForeignKey('users.u_id'))
+    o_id = db.Column(db.Integer, db.ForeignKey('outcomes.o_id'))
+    c_id = db.Column(db.Integer, db.ForeignKey('contexts.c_id'))
+    ns_id = db.Column(db.Integer, db.ForeignKey('next_steps.ns_id'))
+    ei_id = db.Column(db.Integer, db.ForeignKey('email_items.ei_id'))
+
+class EmailItem(db.Model):
+    """all the parts of emails that come in"""
+    __tablename__ = "email_items"
+    ei_id = db.Column(db.Integer, primary_key=True)
+    sender = db.Column(db.Text, nullable=False)
+    send_to = db.Column(db.ARRAY(db.Text), nullable=False)
+    cc = db.Column(db.ARRAY(db.Text), nullable=False)
+    bcc = db.Column(db.ARRAY(db.Text), nullable=False)
+    subject = db.Column(db.Text, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    html = db.Column(db.Text, nullable=False)
+
+class NextAction(db.Model):
+    """the next thing a user should be doing"""
+    __tablename__ = "next_actions"
+    na_id = db.Column(db.Integer, primary_key=True)
+    i_id = db.Column(db.Integer, db.ForeignKey('items.i_id'), nullable=False)
+    dt_moved = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    last_viewed = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class SomedayMaybe(db.Model):
+    """things the user may get around to one day"""
+    __tablename__ = "someday_maybe"
+    sm_id = db.Column(db.Integer, primary_key=True)
+    i_id = db.Column(db.Integer, db.ForeignKey('items.i_id'), nullable=False)
+    dt_moved = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    last_viewed = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Calendar(db.Model):
+    """date or time specfic calendar items"""
+    __tablename__ = "calendar"
+    cal_id = db.Column(db.Integer, primary_key=True)
+    i_id = db.Column(db.Integer, db.ForeignKey('items.i_id'), nullable=False)
+    day = db.Column(db.Boolean, nullable=False, default=False)
+    dt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    dt_moved = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    last_viewed = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class WaitingOn(db.Model):
+    """Things that the user is waiting on"""
+    __tablename__ = "waiting_on"
+    wo_id = db.Column(db.Integer, primary_key=True)
+    i_id = db.Column(db.Integer, db.ForeignKey('items.i_id'), nullable=False)
+    dt_moved = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    last_viewed = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Reference(db.Model):
+    """General library of non-actionable things"""
+    __tablename__ = "reference"
+    r_id = db.Column(db.Integer, primary_key=True)
+    i_id = db.Column(db.Integer, db.ForeignKey('items.i_id'), nullable=False)
+    dt_moved = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    last_viewed = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Context(db.Model):
+    """who are you with? where are you? is your kid with you?
+    The general scenario in which you should be in order to complete the task"""
+    __tablename__ = "contexts"
+    c_id = db.Column(db.Integer, primary_key=True)
+    c_name = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.Boolean, nullable=False, default=False)
+
+class WorkMode(db.Model):
+    """will determine the views / modes a user will be in while completing
+    'work'. Can either  be "Doing predefined work", "Doing work as it shows up", 
+    or "Defining your work"."""
+    __tablename__ = "work_modes"
+    wm_id = db.Column(db.Integer, primary_key=True)
+    wm_name = db.Column(db.Text, nullable=False)
+    wm_view = db.Column(db.Text)
